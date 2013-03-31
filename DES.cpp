75ec7741faca6edde2478cdc16548386fd12320b
Anirban Das <anirban.nick@gmail.com>
2013-03-31 19:17:27 +0530

    Added comments and directions of use

---
+++ DES.cpp
@@ -0,0 +1,31 @@
+/*  A program that encrypts a file using the DES algorthim
+     that has been implemented here in very basic level
+
+
+      Created By ,   Anirban Das  anirban.nick@gmail.com
+ 
+      2013
+
+      How to use :
+  
+      type :
+   
+      $ DES -E filename.extension key   (for Encrypting)
+      $ DES -D fiename.des key  (for Decrypting)
+
+ -    you require the file to be encrypted along with the key you want 
+      to use to encrypt the file.
+
+  -   you require the file to be decrypted along with the key you want
+      that was used to encrypt the file with. 
+
+  NOTE :  the encrypted the file has .des extension
+          Thus, for decrypting the file should be a .des file
+
+*/
+       
+     
+
+
+
+
